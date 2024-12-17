4d0432e08b2564878cd87929ee91cc7385675cb3
kgruiz <kgruiz@umich.edu>
2024-12-17 17:07:34 -0500

    Reformatted Images Directory

--- Utils/ImageIndexer.py
+++ Utils/ImageIndexer.py
@@ -1,57 +1,130 @@
 import json
 import os
+from pathlib import Path
 
 from PIL import Image
 from PIL.ExifTags import TAGS
 
 
 class ImageMetadataExtractor:
-    def __init__(self, directory):
-        self.directory = directory
-        self.supportedFormats = (".jpeg", ".jpg", ".png", ".svg")
-        self.outputFile = "ImageIndex.xml"
 
-    def ExtractMetadata(self):
+    def __init__(
+        self, directory: str, outputEmptyMetadata: bool = True, outputAsXml: bool = True
+    ):
+        self.currentDir: Path = Path("..").resolve()
+        self.directory: Path = self.currentDir / Path(directory)
+        self.supportedFormats: tuple[str, str, str, str] = (
+            ".jpeg",
+            ".jpg",
+            ".png",
+            ".svg",
+        )
+        self.outputFile: str = "ImageIndex." + ("xml" if outputAsXml else "json")
+        self.outputEmptyMetadata: bool = outputEmptyMetadata
+        self.outputAsXml: bool = outputAsXml
+
+    def ExtractMetadata(self) -> None:
+        if self.outputAsXml:
+            self._extractMetadataXml()
+        else:
+            self._extractMetadataJson()
+
+    def _extractMetadataXml(self) -> None:
         with open(self.outputFile, "w") as file:
             file.write('<?xml version="1.0" encoding="UTF-8"?>\n<Images>\n')
             for root, _, files in os.walk(self.directory):
                 for fileName in files:
                     if fileName.lower().endswith(self.supportedFormats):
-                        filePath = os.path.join(root, fileName)
-                        metadata = self.GetImageMetadata(filePath)
+                        filePath: Path = Path(root) / fileName
+                        if "MPhotos" not in filePath.as_posix():
+                            continue
+                        relativePath: Path = filePath.relative_to(self.currentDir)
+                        metadata: dict[str, str] | str = self.GetImageMetadata(filePath)
                         if (
                             fileName.lower().endswith(".svg")
                             or metadata == "No EXIF metadata found"
+                            or len(metadata) == 0
                         ):
-                            file.write(
-                                f"  <Image>\n    <Path>{filePath}</Path>\n    <Name>{fileName}</Name>\n    <Metadata>None</Metadata>\n  </Image>\n\n"
-                            )
-                            print(
-                                f"Path: {filePath}\nName: {fileName}\nMetadata: None\n\n"
-                            )
+                            if self.outputEmptyMetadata:
+                                file.write(
+                                    f"  <Image>\n    <Path>{relativePath}</Path>\n    <Name>{fileName}</Name>\n    <Metadata>None</Metadata>\n  </Image>\n\n"
+                                )
+                                print(
+                                    f"Path: {relativePath}\nName: {fileName}\nMetadata: None\n\n"
+                                )
+                            else:
+                                file.write(
+                                    f"  <Image>\n    <Path>{relativePath}</Path>\n    <Name>{fileName}</Name>\n  </Image>\n\n"
+                                )
+                                print(f"Path: {relativePath}\nName: {fileName}\n\n")
                         else:
-                            metadataXml = "\n      ".join(
+                            metadataXml: str = "\n      ".join(
                                 [
                                     f"<{key}>{value}</{key}>"
                                     for key, value in metadata.items()
                                 ]
                             )
                             file.write(
-                                f"  <Image>\n    <Path>{filePath}</Path>\n    <Name>{fileName}</Name>\n    <Metadata>\n      {metadataXml}\n    </Metadata>\n  </Image>\n\n"
+                                f"  <Image>\n    <Path>{relativePath}</Path>\n    <Name>{fileName}</Name>\n    <Metadata>\n      {metadataXml}\n    </Metadata>\n  </Image>\n\n"
                             )
                             print(
-                                f"Path: {filePath}\nName: {fileName}\nMetadata: {metadataXml}\n\n"
+                                f"Path: {relativePath}\nName: {fileName}\nMetadata: {metadataXml}\n\n"
                             )
             file.write("</Images>")
 
-    def GetImageMetadata(self, filePath):
+    def _extractMetadataJson(self) -> None:
+        with open(self.outputFile, "w") as file:
+            images = []
+            for root, _, files in os.walk(self.directory):
+                for fileName in files:
+                    if fileName.lower().endswith(self.supportedFormats):
+                        filePath: Path = Path(root) / fileName
+                        if "MPhotos" not in filePath.as_posix():
+                            continue
+                        relativePath: Path = filePath.relative_to(self.currentDir)
+                        metadata: dict[str, str] | str = self.GetImageMetadata(filePath)
+                        if (
+                            fileName.lower().endswith(".svg")
+                            or metadata == "No EXIF metadata found"
+                            or len(metadata) == 0
+                        ):
+                            if self.outputEmptyMetadata:
+                                images.append(
+                                    {
+                                        "Path": str(relativePath),
+                                        "Name": fileName,
+                                        "Metadata": "None",
+                                    }
+                                )
+                                print(
+                                    f"Path: {relativePath}\nName: {fileName}\nMetadata: None\n\n"
+                                )
+                            else:
+                                images.append(
+                                    {"Path": str(relativePath), "Name": fileName}
+                                )
+                                print(f"Path: {relativePath}\nName: {fileName}\n\n")
+                        else:
+                            images.append(
+                                {
+                                    "Path": str(relativePath),
+                                    "Name": fileName,
+                                    "Metadata": metadata,
+                                }
+                            )
+                            print(
+                                f"Path: {relativePath}\nName: {fileName}\nMetadata: {metadata}\n\n"
+                            )
+            json.dump({"Images": images}, file, indent=4)
+
+    def GetImageMetadata(self, filePath: Path) -> dict[str, str] | str:
         try:
             image = Image.open(filePath)
             info = image._getexif()
             if info is not None:
                 desiredTags = [
-                    "ResolutionUnit",
-                    "ImageDescription",
+                    "Make",
+                    "Model",
                     "DateTime",
                     "Orientation",
                     "Artist",
@@ -69,5 +142,7 @@ class ImageMetadataExtractor:
 
 
 if __name__ == "__main__":
-    extractor = ImageMetadataExtractor("assets/")
+    extractor = ImageMetadataExtractor(
+        "assets/", outputEmptyMetadata=True, outputAsXml=True
+    )
     extractor.ExtractMetadata()
