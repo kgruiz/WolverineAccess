3a34f2434e65269b2000026d927171d28322787f
kgruiz <kgruiz@umich.edu>
2024-12-17 17:41:13 -0500

    Improved Not Implemented Page

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 window.addEventListener("scroll", () => {
-
-  const backToTopButton = document.querySelector(".back-to-top")
+  const backToTopButton = document.querySelector(".back-to-top");
+  const homeButton = document.querySelector(".home-button");
 
   if (window.scrollY > 200) {
-    backToTopButton.style.display = "block"
+    backToTopButton.style.display = "block";
+    homeButton.style.display = "block";
   } else {
-    backToTopButton.style.display = "none"
+    backToTopButton.style.display = "none";
+    homeButton.style.display = "none";
   }
-
-})
+});
 
 document.querySelector(".back-to-top").addEventListener("click", () => {
-
   window.scrollTo({
     top: 0,
     behavior: "smooth"
-  })
+  });
+});
 
-})
+document.querySelector(".home-button").addEventListener("click", () => {
+  window.location.href = "index.html";
+});
